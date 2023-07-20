eb5ab4004a33950c6648a48134e2568a406b66c0
Ben Campbell <ben.campbell.w@gmail.com>
2023-07-20 19:48:54 -0400

    added fizzbizz solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,17 @@
-# add your code here
+def zznumber(number):
+    #number is divisible by m iff not number % m
+    if not number % 3:
+        if not number % 5:
+            return "FizzBuzz"
+        else:
+            return "Fizz"
+    elif not number % 5:
+        return "Buzz"
+    else:
+        return number
+    
+def main():
+    for n in range(1, 101):
+        print(zznumber(n))
+    
 
