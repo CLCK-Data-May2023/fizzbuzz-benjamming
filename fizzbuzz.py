b5f99195337b0c1daed91764a44edd3ce48ced8d
Ben Campbell <ben.campbell.w@gmail.com>
2023-07-24 18:23:03 -0400

    git practice

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-def zznumber(number):
+def fuzznumber(number):
     #number is divisible by m iff not number % m
     if not number % 3:
         if not number % 5:
@@ -12,7 +12,7 @@ def zznumber(number):
     
 def main():
     for n in range(1, 101):
-        print(zznumber(n))
+        print(fuzznumber(n))
 
 main()
     
